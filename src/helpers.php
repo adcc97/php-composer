6024a171f12a9cbc548b4b7ad5eae0cb7522fabb
DavidCastillo <adcc.97@outlook.com>
2021-08-30 16:31:42 -0500

    php con composer

---
+++ src/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+
+if (! function_exists('upper')) {
+
+     function upper($value){
+
+          return Text\Format::upperText($value);
+     }
+     
+     # code...
+}
+
+
+if (! function_exists('lower')) {
+function lower($value){
+
+     return Text\Format::lowerText($value);
+     }
+}
